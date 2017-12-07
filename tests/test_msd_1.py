b43a8bad7871e1bdc106de797647d840da86f7e2
Pierre de Buyl <pdebuyl@pdebuyl.be>
2017-12-07 22:05:07 +0100

    add test_msd_1

---
+++ tests/test_msd_1.py
@@ -0,0 +1,16 @@
+import numpy as np
+import tidynamics
+
+def test_linear_msd():
+    N = 100
+    time = np.arange(N)
+    reference_msd = time**2
+    computed_msd = tidynamics.msd(time.reshape((-1,1)))
+    assert np.allclose(reference_msd, computed_msd)
+
+def test_cst_msd():
+    N = 100
+    data = np.ones(N)
+    reference_msd = np.zeros_like(data)
+    computed_msd = tidynamics.msd(data.reshape((-1,1)))
+    assert np.allclose(reference_msd, computed_msd)
