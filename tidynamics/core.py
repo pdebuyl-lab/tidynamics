4b6cad9694439897968a93ef24a9e0e7f80b76c6
Pierre de Buyl <pdebuyl@pdebuyl.be>
2018-06-29 14:23:19 +0200

    use only powers of two for the fft
    following advice from Max Linke
    https://github.com/pdebuyl-lab/tidynamics/issues/1
    
    resolves #1

--- tidynamics/core.py
+++ tidynamics/core.py
@@ -1,20 +1,40 @@
 from __future__ import division
 import numpy as np
 
+def select_power_of_two(n):
+    """
+    Select the closest i such that n<=2**i
+    """
+    current_exp = int(np.ceil(np.log2(n+1)))
+    if n == 2**current_exp:
+        n_fft = n
+    if n < 2**current_exp:
+        n_fft = 2**current_exp
+    elif n > 2**current_exp:
+        n_fft = 2**(current_exp+1)
+
+    return n_fft
+
+
 def autocorrelation_1d(data):
     """
     Compute the autocorrelation of a scalar time series.
     """
 
     N = len(data)
+    n_fft = select_power_of_two(N)
 
     # Pad the signal with zeros to avoid the periodic images.
-    R_data = np.zeros(2*N)
+
+    R_data = np.zeros(2*n_fft)
     R_data[:N] = data
+
     F_data = np.fft.fft(R_data)
+
     result = np.fft.ifft(F_data*F_data.conj())[:N].real/(N-np.arange(N))
 
-    return result
+    return result[:N]
+
 
 def correlation_1d(data1, data2):
     """
@@ -32,11 +52,12 @@ def correlation_1d(data1, data2):
 
     N = len(data1)
     assert N == len(data2)
+    n_fft = select_power_of_two(N)
 
     # Pad the signal with zeros to avoid the periodic images.
-    R_data1 = np.zeros(2*N)
+    R_data1 = np.zeros(2*n_fft)
     R_data1[:N] = data1
-    R_data2 = np.zeros(2*N)
+    R_data2 = np.zeros(2*n_fft)
     R_data2[:N] = data2
     F_data1 = np.fft.fft(R_data1)
     F_data2 = np.fft.fft(R_data2)
