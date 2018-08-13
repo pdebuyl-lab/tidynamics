e9c0cc45f166a56fe1621be0504882a2aba26d74
Pierre de Buyl <pdebuyl@pdebuyl.be>
2018-08-13 16:23:14 +0200

    add standalone cli tool and example data
    fixes #10

--- examples/README.txt
+++ examples/README.txt
@@ -2,6 +2,20 @@
 Examples of using tidynamics
 ============================
 
-The examples below show how one can use tidynamics to compute correlations and mean-squared
-displacements.
+The examples below show how one can use tidynamics to compute correlations and
+mean-squared displacements (MSD).
 
+The example "Command-line interface to tidynamics" runs in the command-line. It performs the
+two basic operations of tidynamics (autocorrelation function and MSD) using files as input
+data. The file ``random_steps_sample_0.txt.gz`` contains example data for computing the
+autocorrelation function and the file ``random_walk_sample_0.txt.gz`` contains example data
+for computing the MSD.
+
+The following commands run the command-line tool::
+
+    python tidynamics_tool.py msd random_walk_sample_0.txt.gz random_walk_msd_0.txt
+    python tidynamics_tool.py acf random_steps_sample_0.txt.gz random_steps_acf_0.txt
+
+The output is stored in the files ``random_walk_msd_0.txt`` and ``random_steps_acf_0.txt``.
+
+The other examples require `matplotlib <https://matplotlib.org/>`_.
---
+++ examples/tidynamics_tool.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+"""
+====================================
+Command-line interface to tidynamics
+====================================
+
+The program reads in data from the file input_file (using np.loadtxt from
+NumPy) and performs the selected action on the data, chosen in:
+
+* acf for the autocorrelation function
+* msd for the mean-square displacement
+
+The program writes the result in the file output_file using the function
+np.savetxt from NumPy.
+
+For more information, consult the documentation of tidynamics at
+http://lab.pdebuyl.be/tidynamics/
+
+"""
+from __future__ import print_function, division
+
+import argparse
+import numpy as np
+import tidynamics
+
+
+parser = argparse.ArgumentParser(description=__doc__,
+                        formatter_class=argparse.RawDescriptionHelpFormatter)
+parser.add_argument('action',
+                    help='Choice of computation to perform on input file',
+                    choices=['acf', 'msd'])
+parser.add_argument('input_file',
+                    help='Filename for input data')
+parser.add_argument('output_file',
+                    help='Filename for the result of the computation')
+args = parser.parse_args()
+
+input_data = np.loadtxt(args.input_file)
+
+if args.action == 'acf':
+    result = tidynamics.acf(input_data)
+elif args.action == 'msd':
+    result = tidynamics.msd(input_data)
+
+np.savetxt(args.output_file, result)
